b779dc70501f5979fefb2f831a33889ecd6d9012
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-01-21 16:12:38 +0200

    Finish Structure, Add tabs Add sample view for notification, chat and account view controllers

---
+++ iOS-Sample-mvvm/Classes/ViewRelated/Scenes/AccountScene/View/AccountViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AccountViewController.swift
+//  iOS-Sample-mvvm
+//
+//  Created by Ahmed Ali on 21/01/2021.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+    }
+
+}
+// MARK: - Configuration
+//
+private extension AccountViewController {
+    
+    /// Configure View
+    ///
+    func configureView() {
+        self.title = Strings.title
+    }
+}
+
+// MARK: - Strings
+//
+private extension AccountViewController {
+    
+    enum Strings {
+        static let title = "Account"
+    }
+}
