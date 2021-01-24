f091a1337cf19872b03c4b8692fb29e744b98137
ayaemahmoud <ayae.mahmoud@gmial.com>
2021-01-24 11:13:33 +0200

    CardDesign Modify cancel part in design Add outlets for card objects

--- iOS-Sample-mvvm/Classes/ViewRelated/Scenes/HomeScene/View/CardCollectionViewCell.swift
+++ iOS-Sample-mvvm/Classes/ViewRelated/Scenes/HomeScene/View/CardCollectionViewCell.swift
@@ -9,8 +9,20 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet private weak var cancel: UIButton!
+    @IBOutlet private weak var wayOfCommunication: UILabel!
+    @IBOutlet private weak var date: UILabel!
+    @IBOutlet private weak var userRating: UIView!
+    @IBOutlet private weak var userDepartment: UILabel!
+    @IBOutlet private weak var userName: UILabel!
+    @IBOutlet private weak var userImage: UIImageView!
+    @IBOutlet private weak var rootView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
+    }
+    
+    @IBAction func cancelAppointment(_ sender: UIButton) {
     }
 }
