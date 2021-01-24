c9154911964be4a3f4c4774429bea9d7cfb9e18b
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-01-24 11:08:04 +0200

    Design header (collection with search bar) - Create search pepole view controller

---
+++ iOS-Sample-mvvm/Classes/ViewRelated/Scenes/SearchPeopleScene/View/SearchPepoleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchPepoleViewController.swift
+//  iOS-Sample-mvvm
+//
+//  Created by Ahmed Ali on 24/01/2021.
+//
+
+import UIKit
+
+class SearchPepoleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
