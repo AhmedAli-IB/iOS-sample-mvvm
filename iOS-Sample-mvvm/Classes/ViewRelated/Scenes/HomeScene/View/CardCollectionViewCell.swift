386b210ddb0284e673e0992f52a16f43db4bd90f
ayaemahmoud <ayae.mahmoud@gmial.com>
2021-01-24 10:29:04 +0200

    Card Design Add card assets, colors, icons Add card file and xib

---
+++ iOS-Sample-mvvm/Classes/ViewRelated/Scenes/HomeScene/View/CardCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CardCollectionViewCell.swift
+//  iOS-Sample-mvvm
+//
+//  Created by Aya Essam on 24/01/2021.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
