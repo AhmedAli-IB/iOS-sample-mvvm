2f57043578281f41730785fb38e5334f0e2ac7a9
ayaemahmoud <ayae.mahmoud@gmial.com>
2021-01-25 10:52:07 +0200

    Bottom Cards Add bottom card design and xib Add assets for bottom cards

--- iOS-Sample-mvvm/Classes/ViewRelated/Scenes/HomeScene/View/Session/SessionTableViewCell.swift
+++ iOS-Sample-mvvm/Classes/ViewRelated/Scenes/HomeScene/View/Session/SessionTableViewCell.swift
@@ -9,17 +9,17 @@ import UIKit
 
 class SessionTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var description: UILabel!
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var icon: UIImageView!
-    @IBOutlet weak var contentView: UIView!
+    @IBOutlet private weak var cellDescription: UILabel!
+    @IBOutlet private weak var cellTitle: UILabel!
+    @IBOutlet private weak var cellIcon: UIImageView!
+    @IBOutlet private weak var rootView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     func setupLabelFonts() {
-        title.font = UIFont(font: FontFamily._29LTAzer.medium, size: 20)
-        description.font = UIFont(font: FontFamily._29LTAzer.regular, size: 15)
+        cellTitle.font = UIFont(font: FontFamily._29LTAzer.medium, size: 20)
+        cellDescription.font = UIFont(font: FontFamily._29LTAzer.regular, size: 15)
     }
 }
