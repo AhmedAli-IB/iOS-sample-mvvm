e713ad688750845acc2de8eae640e6bd3c917198
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-02-01 11:06:05 +0200

    Filtration Subject - Add FieldsViewController

---
+++ iOS-Sample-mvvm/Classes/ViewRelated/Scenes/FieldsScene/View/FieldsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FieldsViewController.swift
+//  iOS-Sample-mvvm
+//
+//  Created by Ahmed Ali on 01/02/2021.
+//
+
+import UIKit
+
+// MARK: - FieldsViewController
+/// `FieldsViewController`  responsible for filter contributors with fields
+//
+class FieldsViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    //
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    // MARK: - Lifecycle
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
