b5458de462723afeacffb30e1e68d689eec7ec6d
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-02-01 15:02:02 +0200

    Centers Filtration - Add CentersViewController

---
+++ iOS-Sample-mvvm/Classes/ViewRelated/Scenes/CentersScene/View/CentersViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CentersViewController.swift
+//  iOS-Sample-mvvm
+//
+//  Created by Ahmed Ali on 01/02/2021.
+//
+
+import UIKit
+
+class CentersViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    //
+    @IBOutlet private weak var tableView: UITableView!
+    
+    // MARK: - Lifecycel
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
